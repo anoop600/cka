e5d015f2683978b6ebca09ce53cd3b4191f0b6ff
anoop <anoop.jain10@gmail.com>
2024-10-12 18:46:09 +0530

    First COmmit

---
+++ .vimrc
@@ -0,0 +1,1 @@
+set et ai ci nu sw=2 ts=2 sts=2
---
+++ script.sh
@@ -0,0 +1,56 @@
+alias kg="k get"
+alias kd="k describe"
+alias kr="k run"
+alias kcr="k create"
+alias ka="k apply -f"
+alias krm="k delete"
+alias k="k "
+alias kn="k config set-context --current --namespace"
+export dry="--dry-run=client -o yaml"
+alias watchall="watch -n0 kubectl get all,secret,cm,netpol,pvc -o wide --show-labels"
+alias watchns="watch -n0 kubectl get ns --show-labels"
+alias watchnodes="watch -n0 kubectl get no --show-labels"
+alias watchpv="watch -n0 kubectl get pv --show-labels"
+alias watchcontext="watch -n0 kubectl config get-contexts"
+alias kevent="kubectl get ev -A -w"
+
+debug() {
+  # Check if the namespace is provided
+  if [ -z "$1" ]; then
+    echo "Usage: debug <namespace>"
+    return 1
+  fi
+
+  NAMESPACE=$1
+
+  # Run the kubectl command with the provided namespace
+  kubectl run temp --rm -it --image nginx:alpine --restart Never --namespace "$NAMESPACE" -- sh
+}
+
+execpod() {
+  # Check if the namespace is provided
+  if [ -z "$1" ]; then
+    echo "Usage: execpod <namespace> <podname>"
+    return 1
+  fi
+  #Checf if the pod name is provided
+  if [ -z "$2" ]; then
+    echo "Usage: execpod <namespace> <podname>"
+    return 1
+  fi
+
+  NAMESPACE=$1
+  PODNAME=$2
+
+  # Run the kubectl command with the provided namespace
+  kubectl exec "PODNAME" -it --namespace "$NAMESPACE" -- sh
+}
+
+alias k=kubectl
+export now="--now=true"
+alias hosts="cat /etc/hosts"
+alias os="cat /etc/*release*"
+alias debugwget="echo wget -O- -T 2 <target host>"
+alias debugcurl="echo curl --connect-timeout 2 <target host>"
+alias debugnc="echo nc -w 2 <target destination> <target port>"
+
